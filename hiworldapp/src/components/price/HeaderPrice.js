fc2a2f44269ddfd51779be49343d58d148082cb7
ilyas <aderiho.justdev@gmail.com>
2018-03-16 00:57:48 +0300

    clear some unnecessary files

--- hiworldapp/src/components/price/HeaderPrice.js
+++ hiworldapp/src/components/price/HeaderPrice.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-
 import PriceBlock from './PriceBlock';
 import './HeaderPrice.css';
 
--- hiworldapp/src/components/price/Price.js
+++ hiworldapp/src/components/price/Price.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import HeaderPrice from './HeaderPrice';
 import Contact from '../common/Contact';
-// import HeaderTr from './HeaderTr';
 
 const Price = () => <div>
     <HeaderPrice/>
     <Contact/>
 </div>
-
 
 export default Price
