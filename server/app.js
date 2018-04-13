212c205da0c16b14bceb778fcaa4adfaa1880d6e
Ilya Aderiho <usc-mwnj@us.ibm.com>
2018-04-13 12:58:37 +0300

    noSleep heroku

--- server/app.js
+++ server/app.js
@@ -40,11 +40,11 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
 
-setInterval(request('https://safe-caverns-88374.herokuapp.com/images/logo.png', function (error, response, body) {
+setInterval(() => {request('https://safe-caverns-88374.herokuapp.com/images/logo.png', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
-}), 30000);
+})}, 30000);
 
 app.post('/message', sendMail);
 
